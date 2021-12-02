8153be434177a7fde2352836a4ebf2d37dc8cd40
Tammy Fina <finatammy@gmail.com>
2021-12-02 14:23:36 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,3 @@ var currentDate = (moment().format('dddd, MM/DD/YYYY'));
 console.log("Moment Date: " + currentDate);
 
 document.getElementById('currentDay').innerHTML=currentDate;
-var tasksEl = document.querySelector(#tasks);
