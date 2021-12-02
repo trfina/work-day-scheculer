c8b6ac23333582ea75c4dc3510727eec54e31ff2
Tammy Fina <finatammy@gmail.com>
2021-12-02 14:18:45 -0500

    creating container elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,4 @@ var currentDate = (moment().format('dddd, MM/DD/YYYY'));
 console.log("Moment Date: " + currentDate);
 
 document.getElementById('currentDay').innerHTML=currentDate;
+var tasksEl = document.querySelector(#tasks);
