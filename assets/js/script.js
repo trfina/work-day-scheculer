2533d26b03b13ff2cdb8ded6010a7f27022442e8
Tammy Fina <finatammy@gmail.com>
2021-12-02 21:56:52 -0500

    heml content added

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,55 +3,47 @@ console.log("Moment Date: " + currentDate);
 
 document.getElementById('currentDay').innerHTML=currentDate;
 
-var scheduleListEl = document.querySelector('#schedule-list');
-var taskHourEl = document.querySelector("#child-left");
-var taskToDoEl = document.querySelector('#child-center');
-var taskBtnEl = document.querySelector("child-right");
+var mainContainerEl = document.querySelector('#main-container');
 
 let schHour  = [' 9 am', '10 am', '11 am', '12pm', ' 1 pm', ' 2 pm', ' 3pm', ' 4 pm', ' 5 pm'];
 let schTask = "Enter task here";
 let schBtn = "Button";
 
+//
+var htmlContent = '';
+for(let i = 0; i < 9 ; i++){
+    htmlContent+= ` <div class="row" id="schedule-list">
+    <div class="col, hour" id="child-left" >hour${schTask}</div>
+    <div class="col-8" id="child-center">hi</div>
+    <div class="col, saveBtn" id="child-right">there</div>
+    </div>   `
+}
+mainContainerEl.innerHTML=htmlContent;
 
+// var parentDiv = document.createElement('div');
+// var rowHTML = ` <div class="row" id="schedule-list">
+//                 <div class="col, hour" id="child-left" >hour</div>
+//                 <div class="col-8" id="child-center">hi</div>
+//                 <div class="col, saveBtn" id="child-right">there</div>
+//                 </div>   `
+// parentDiv.innerHTML = rowHTML;
+// mainContainerEl.append(rowHTML);
+
+ // parentDiv.innerHTML = '<div class="col, hour" id="child-left">' + schHour[i] + '</div>'; 
+    
+//    // parentDiv = document.createElement('div');
+//     parentDiv.className = "row";
+//     parentDiv.id = "schedule-list";
+//     console.log(schTask);
+//     parentDiv.innerHTML = '<div class="col-8"  id="child-center">' + schTask+ '</div>';
+
+//     parentDiv = document.createElement('div');
+//     parentDiv.className = "row";
+//     parentDiv.id = "schedule-list";
+//     console.log(schBtn);
+//     parentDiv.innerHTML = '<div class="col, hour" id="child-right">' + schBtn + '</div>';
 //var toAdd = document.createDocumentFragment();
 
 for (var i = 0; i < schHour.length; i++){
-    var parentDiv = document.createElement('div');
-    parentDiv.className = "row";
-    parentDiv.id = "schedule-list";
-    console.log(schHour[i]);
-    parentDiv.innerHTML = '<div class="col, hour" id="child-left">' + schHour[i] + '</div>'; 
     
-    parentDiv = document.createElement('div');
-    parentDiv.className = "row";
-    parentDiv.id = "schedule-list";
-    console.log(schTask);
-    parentDiv.innerHTML = '<div class="col-8"  id="child-center">' + schTask+ '</div>';
-
-    parentDiv = document.createElement('div');
-    parentDiv.className = "row";
-    parentDiv.id = "schedule-list";
-    console.log(schBtn);
-    parentDiv.innerHTML = '<div class="col, hour" id="child-right">' + schBtn + '</div>';
-    //let newDivParent = document.create
-    //let newDiv = document.createElement('div');
-    //newDiv.id = "child-left";
-    //newDiv.className = "col, hour";
-    //toAdd.appendChild(newDiv);
-    //console.log(newDiv);
-    
-    
-    //console.log(div);
-    //div.textContent = schHour[i];
-    //console.log(div.textContent);
-    //scheduleListEl.appendChild(div);
-    //div.textContent = schTask;
-    //console.log(div.textContent);
-    //scheduleListEl.appendChild(div);
-    //console.log(div.textContent);
-    //div.texContent = schBtn;
-    //scheduleListEl.appendChild(div);
-   // console.log(div.textContent);
 }
-
-//document.appendChild(toAdd);
